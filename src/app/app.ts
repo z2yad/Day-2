a6c1e1a34d03f777fc8bcaa5b2ab9c94ef45a945
zeyad salim <ziadsalim121@gmail.com>
2026-07-14 16:36:13 +0300

    feat: implement sidebar component and integrate it into the main application layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "./shared/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
