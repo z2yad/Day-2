f4359a00f02715e7793ce3d271923a21a999db73
zeyad salim <ziadsalim121@gmail.com>
2026-07-15 15:47:00 +0300

    feat: initialize dashboard structure with routing, layout, and sidebar components

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Dashbord } from '../../feature/dashbord/dashbord';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink,Dashbord],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
